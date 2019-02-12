8a4ee404c60fb3c2984a974319aca6256ddec11c
Rasheed Rahman <ra1da35ma@gmail.com>
2019-02-12 15:35:17 +0100

    remove template ref

--- src/wizard-step.component.ts
+++ src/wizard-step.component.ts
@@ -1,52 +1,31 @@
-import { AfterContentInit, Component, ContentChild, EventEmitter, Input, Output, TemplateRef } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import {AfterContentInit, Component, ContentChild, EventEmitter, Input, Output} from '@angular/core';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'wizard-step',
   template:
         `
 		<div [hidden]="!isActive">
-			<ng-container *ngIf="templateContent else content">
-				<ng-template [ngTemplateOutlet]="templateContent"></ng-template>
-
-			</ng-container>
-
-			<ng-template #content>
-				<ng-content></ng-content>
-			</ng-template>
+			<ng-content></ng-content>
 		</div>
   `
 })
 export class WizardStepComponent implements AfterContentInit {
   @Input() title: string;
   @Input() hidden: boolean = false;
+  @Input() isValid: boolean = true;
   @Input() showNext: boolean = true;
   @Input() showPrev: boolean = true;
-  @Input() templateContent: TemplateRef<void>;
 
   @ContentChild(NgForm) form: NgForm;
 
   @Output() onNext: EventEmitter<any> = new EventEmitter<any>();
   @Output() onPrev: EventEmitter<any> = new EventEmitter<any>();
   @Output() onComplete: EventEmitter<any> = new EventEmitter<any>();
+
   isDisabled: boolean = true;
 
   constructor() {
-  }
-
-  private _isValid: boolean = true;
-
-  get isValid(): boolean {
-    if (this.form) {
-      return this.form.form.valid;
-    }
-
-    return this._isValid;
-  }
-
-  @Input('isValid')
-  set isValid(value: boolean) {
-    this._isValid = value;
   }
 
   private _isActive: boolean = false;
@@ -62,8 +41,9 @@ export class WizardStepComponent implements AfterContentInit {
   }
 
   ngAfterContentInit(): void {
+    console.log(this.form);
     if (this.form) {
-      this._isValid = this.form.form.valid;
+      this.isValid = this.form.form.valid;
     }
   }
 }
